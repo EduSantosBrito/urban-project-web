96b42df8008fa2aa29ca38428268e23a56a3fd89
Eduardo Brito <edu.santos.brito@gmail.com>
2018-11-29 23:28:31 -0200

    Login com autenticação feito

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,6 +1,32 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./Login.css";
 export default class Login extends Component {
+  handleEmailChange = e => {
+    this.setState({ email: e.target.value });
+  };
+
+  handlePasswordChange = e => {
+    this.setState({ password: e.target.value });
+  };
+
+  handleClick = () => {
+    let user = { email: this.state.email, password: this.state.password };
+    axios
+      .post("http://localhost:8081/users/login/", user, {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+      })
+      .then(res => {
+        localStorage.setItem("tokenAuth", res.data);
+      })
+      .catch(err => {
+        alert(err.response.data.message);
+      });
+  };
+
   render() {
     return (
       <div className="site">
@@ -12,16 +38,21 @@ export default class Login extends Component {
           />
           <h1>Log into your account</h1>
           <div className="form">
-            <input className="form__input_username" type="text" required />
-            <span className="form__label_username">Your Username</span>
-            <input className="form__input_password" type="password" required />
-            <span className="form__label_password">Password</span>
-            <a href="https://www.google.com">
-              <button>Log in</button>
-            </a>
-            <a href="https://www.google.com">
-              <p>Forgot your password?</p>
-            </a>
+            <input
+              className="form__input"
+              placeholder="Your email"
+              type="text"
+              onChange={this.handleEmailChange}
+              required
+            />
+            <input
+              className="form__input"
+              placeholder="Your password"
+              type="password"
+              onChange={this.handlePasswordChange}
+              required
+            />
+            <button onClick={this.handleClick}>Log in</button>
           </div>
         </main>
         <footer>
